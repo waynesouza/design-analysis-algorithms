878cdb62a5f1e4b632340836dcc6b26b21eb9c18
waynesouza <waynesouza07@gmail.com>
2023-06-22 23:10:29 -0300

    fix: some fixes

--- dynamic_programming/supermarket/main.py
+++ dynamic_programming/supermarket/main.py
@@ -1,21 +1,52 @@
-while True:
-    M, N = map(int, input().split())
-    if M == 0 and N == 0:
-        break
+import sys
 
-    Xi = list(map(int, input().split()))
 
-    dp = [float('inf')] * (N * M)
-    dp[0] = 0
+def solve_shopping_session(M, N, jones_list):
+    # Inicializar a tabela de custos mínimos
+    # O valor inicial será infinito (representado por sys.float_info.max)
+    dp = [[sys.float_info.max] * (N + 1) for _ in range(M + 1)]
 
-    for _ in range(N):
-        K, P = map(float, input().split())
-        for j in range(1, M + 1):
-            if K == Xi[j - 1] and dp[j - 1] != float('inf') and (dp[j - 1] + P) < dp[j]:
-                dp[j] = dp[j - 1] + P
-                break
+    # Preencher a primeira linha com zeros, pois é possível comprar zero itens
+    supermarket_list = []
+    for j in range(N + 1):
+        dp[0][j] = 0
+        if j != N:
+            K, P = map(float, input().split())
+            supermarket_list.append((int(K), float(P)))
 
-    if dp[M] == float('inf'):
-        print("Impossible")
+    # Preencher a tabela de custos mínimos
+    for i in range(1, M + 1):
+        for j in range(1, N + 1):
+            # Verificar se o produto está disponível no supermercado
+            if jones_list[i - 1] == supermarket_list[j - 1][0]:
+                # Caso o produto esteja disponível, calcular o custo mínimo
+                # entre comprar o produto e não comprar o produto
+                dp[i][j] = min(dp[i - 1][j - 1] + supermarket_list[j - 1][1], dp[i][j - 1])
+            else:
+                # Caso o produto não esteja disponível, copiar o custo mínimo
+                # da linha anterior
+                dp[i][j] = dp[i][j - 1]
+
+    # Verificar se é possível comprar todos os itens da sessão
+    if dp[M][N] == sys.float_info.max:
+        return "Impossible"
     else:
-        print(f"{dp[M]:.2f}")
+        return "{:.2f}".format(dp[M][N])
+
+
+# Função principal
+def main():
+    while True:
+        M, N = map(int, input().split())
+        if M == 0 and N == 0:
+            break
+
+        jones_list = list(map(int, input().split()))
+
+        result = solve_shopping_session(M, N, jones_list)
+        print(result)
+
+
+# Executar a função principal
+if __name__ == "__main__":
+    main()
